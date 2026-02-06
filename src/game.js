919fd71d0fbcdae63d28678d3485436432a9ce7b
AntiBlaze <175415025+AntiBlaze1@users.noreply.github.com>
2026-02-06 17:49:41 -0500

    Did some stuff

---
+++ src/game.js
@@ -0,0 +1,38 @@
+var iceCream=0;
+
+document.addEventListener("DOMContentLoaded",function () {
+    updateIceCreamCounter();
+
+
+    //When button clicked
+    var clickButton=document.getElementById("clicker");
+    clickButton.onclick=function(e) {
+        iceCream++;
+        updateIceCreamCounter();
+        clickButton.style.animation="none";
+        void clickButton.offsetWidth;
+        clickButton.style.animation="scaleClicker 0.25s";
+    }
+
+});
+
+function updateIceCreamCounter() {
+    document.getElementById("counter").innerHTML=getIceCreamAsWord();
+}
+
+const numSuffix= new Map();
+numSuffix.set(2,"million");
+numSuffix.set(3,"billion");
+numSuffix.set(4,"trillion");
+numSuffix.set(5,"quadrillion");
+numSuffix.set(6,"quintillion");
+numSuffix.set(7,"sextillion");
+
+function getIceCreamAsWord() {
+    var digits=iceCream.toString().length;
+    var threeZeroes=Math.floor((digits-1)/3);
+    var prefix=iceCream.toString().slice(0,2)/10;
+    return (numSuffix.get(threeZeroes)!=null)?prefix+" "+numSuffix.get(threeZeroes):iceCream;
+}
+
+
