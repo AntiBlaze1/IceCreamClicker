8670451403536e8041d4561fe0195c8e83cd83ae
AntiBlaze <175415025+AntiBlaze1@users.noreply.github.com>
2026-02-08 21:23:31 -0500

    more stuff

--- src/game.js
+++ src/game.js
@@ -58,12 +58,6 @@ const shopEffects=Object.freeze({
     INC_SEC:1,
 });
 
-// Name, description, icon path (relative to assets/shopItems), cost, effects
-// For INC_SEC, make sure the lowest decimal is tenths
-const shopItems= [
-    ["Scoop", "scoop", "does stuff ig","scoop.png", 10, [shopEffects.INC_SEC,0.1]],
-
-]
 
 const nameIndex=0;
 const idIndex=1
---
+++ src/shopItems.js
@@ -0,0 +1,6 @@
+// Name, description, icon path (relative to assets/shopItems), cost, effects
+// For INC_SEC, make sure the lowest decimal is tenths
+const shopItems= [
+    ["Scoop", "scoop", "does stuff ig","scoop.png", 10, [shopEffects.INC_SEC,0.1]],
+
+]
